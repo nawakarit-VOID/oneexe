bbad3fd2fee956bd59fd7baba6349e4b1abd285c
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-04-20 22:08:24 +0700

    *8

--- buildexe.sh
+++ buildexe.sh
@@ -6,21 +6,54 @@ echo "🔧 build resource..."
 x86_64-w64-mingw32-windres app.rc -O coff -o rsrc.syso
 
 echo "📦 build exe..."
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ CC=x86_64-w64-mingw32-gcc \ go build -o app.exe
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ 
+CC=x86_64-w64-mingw32-gcc \ 
+go build -o app.exe
 
 echo "✅ done"
-
+--------------------------sudo apt install mingw-w64 ***-1
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \
+CC=x86_64-w64-mingw32-gcc \
+go build -o app.exe
+-----------------------
 
 2-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ 
 CC=x86_64-w64-mingw32-gcc \ 
 go build -o app.exe
 
-3-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ CC=x86_64-w64-mingw32-gcc \ go build -o app.exe
+3-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ 
+CC=x86_64-w64-mingw32-gcc \ 
+go build -o app.exe
 
 4-CC=x86_64-w64-mingw32-gcc fyne package -os windows
 
-5-rsrc -manifest app.manifest -ico icon.ico -o rsrc.syso
+***5-rsrc -manifest app.rc -ico icon.ico -o rsrc.syso
 go build -o app.exe
 
-6-x86_64-w64-mingw32-windres app.rc -O coff -o rsrc.syso
+***6-x86_64-w64-mingw32-windres app.rc -O coff -o rsrc.syso
+
 fyne package -os windows
+
+--x86_64-w64-mingw32-windres app.rc -O coff -o rsrc.syso
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ 
+CC=x86_64-w64-mingw32-gcc \ 
+go build -o app.exe
+
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 
+CC=x86_64-w64-mingw32-gcc 
+go build -o app.exe
+
+CC=x86_64-w64-mingw32-gcc fyne package -os windows
+
+fyne package -os windows
+
+#!/bin/bash
+7--
+echo "🔧 build resource..."
+x86_64-w64-mingw32-windres app.rc -O coff -o rsrc.syso
+
+echo "📦 build exe..."
+CC=x86_64-w64-mingw32-gcc fyne package -os windows
+
+echo "✅ done"
+
