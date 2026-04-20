a9961759be4c376210b4f94d834042624e355a9f
nawakarit <272197659+nawakarit-VOID@users.noreply.github.com>
2026-04-20 20:48:05 +0700

    *7

--- buildexe.sh
+++ buildexe.sh
@@ -6,10 +6,21 @@ echo "🔧 build resource..."
 x86_64-w64-mingw32-windres app.rc -O coff -o rsrc.syso
 
 echo "📦 build exe..."
-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \
-CC=x86_64-w64-mingw32-gcc \
-go build -o app.exe
+GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ CC=x86_64-w64-mingw32-gcc \ go build -o app.exe
 
 echo "✅ done"
 
 
+2-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ 
+CC=x86_64-w64-mingw32-gcc \ 
+go build -o app.exe
+
+3-GOOS=windows GOARCH=amd64 CGO_ENABLED=1 \ CC=x86_64-w64-mingw32-gcc \ go build -o app.exe
+
+4-CC=x86_64-w64-mingw32-gcc fyne package -os windows
+
+5-rsrc -manifest app.manifest -ico icon.ico -o rsrc.syso
+go build -o app.exe
+
+6-x86_64-w64-mingw32-windres app.rc -O coff -o rsrc.syso
+fyne package -os windows
